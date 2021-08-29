c6828d99d5c1307e76f3e53d44a6f09587590507
Dodo <kasper199914@gmail.com>
2021-08-29 16:56:08 +0200

    day 05 rework

--- Zig/src/day05.zig
+++ Zig/src/day05.zig
@@ -2,69 +2,44 @@ const input = @embedFile("../../Inputs/day05.txt");
 const std = @import("std");
 const mem = std.mem;
 
-const Set = enum { LOWER, UPPER, NONE };
+pub fn partition(characters: []const u8) u8 {
+    const len = characters.len;
+    var place = @as(u8, 0);
 
-const Range = struct { lower: u32, upper: u32, last_set: Set = Set.NONE };
+    for (characters) |c, idx| switch (c) {
+        'B', 'R' => place |= @as(u8, 1) << @intCast(u3, len - idx - 1),
+        'F', 'L' => {},
+        else => unreachable,
+    };
+
+    return place;
+}
 
 pub fn main() anyerror!void {
     var lines = mem.split(input, "\r\n");
     var max_seat_id = @as(u32, 0);
+    var boarded: [1000]bool = [1]bool{false} ** 1000;
 
     while (lines.next()) |line| {
         const row_line = line[0..7];
         var column_line = line[7..line.len];
 
-        var row: Range = Range{ .lower = 0, .upper = 127 };
-        var columns = Range{ .lower = 0, .upper = 7 };
+        const row = @intCast(u32, partition(row_line));
+        const column = @intCast(u32, partition(column_line));
+        const seat_id = row * 8 + column;
 
-        for (row_line) |c| {
-            const middle = (row.upper + row.lower) / 2;
-            switch (c) {
-                'F' => {
-                    row.upper = middle;
-                    row.last_set = Set.UPPER;
-                },
-                'B' => {
-                    row.lower = middle + 1;
-                    row.last_set = Set.LOWER;
-                },
-                else => unreachable,
-            }
-        }
-
-        for (column_line) |c| {
-            const middle = (columns.upper + columns.lower) / 2 + 1;
-
-            switch (c) {
-                'L' => {
-                    columns.upper = middle;
-                    columns.last_set = Set.UPPER;
-                },
-                'R' => {
-                    columns.lower = middle;
-                    columns.last_set = Set.LOWER;
-                },
-                else => unreachable,
-            }
-
-        }
-
-        const final_row = switch (row.last_set) {
-            Set.NONE => unreachable,
-            Set.UPPER => row.lower,
-            Set.LOWER => row.upper,
-        };
-
-        const final_column = switch (columns.last_set) {
-            Set.NONE => unreachable,
-            Set.UPPER => columns.lower,
-            Set.LOWER => columns.upper,
-        };
-
-
-        const seat_id = final_row * 8 + final_column;
         if (seat_id > max_seat_id) max_seat_id = seat_id;
+
+        boarded[seat_id] = true;
     }
 
-    std.debug.print("SEAT ID = {}\n", .{max_seat_id});
+    var prv = @as(usize, 0);
+    const boarding_id = for (boarded) |b, idx| {
+        if (!b) {
+            defer prv = idx;
+            if (idx == prv + 1 or idx == 0) continue else break idx;
+        }
+    } else 0;
+
+    std.debug.print("Part 1: {}\nPart 2: {}", .{ max_seat_id, boarding_id });
 }
