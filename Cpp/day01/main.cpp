ea09dca4b8dea49f43d6feee36038f1d5b3e0a4d
DutchGhost <kasper199914@gmail.com>
2021-09-12 16:34:20 +0200

    use std::ranges

--- Cpp/day01/main.cpp
+++ Cpp/day01/main.cpp
@@ -1,50 +1,63 @@
-#include <iostream>
-#include <string>
-#include <ostream>
 #include <fstream>
+#include <iostream>
+#include <ostream>
+#include <ranges>
+#include <string>
 #include <vector>
 
-auto part1(const std::vector<int>& input, int target) noexcept {
-    size_t input_size = input.size();
+template <typename T>
+concept Add = requires(T x) {
+    x + x;
+};
 
-    for(int i = 0; i < input_size; i++) {
-        for(int j = i; j < input_size; j++) {
-            if (input[i] + input[j] == target) {
-                return input[i] * input[j];
-            }
-        }
+template <typename T>
+concept Mul = requires(T x) {
+    x *x;
+};
+
+using namespace std::ranges::views;
+
+template <typename T>
+requires Add<T> && Mul<T>
+auto part1(const std::vector<T> &input, T target) noexcept {
+    auto idx = 0;
+    for (auto a : input) {
+        idx += 1;
+        auto selection =
+            input | drop(idx) |
+            filter([a, target](int b) { return a + b == target; }) |
+            transform([a](int b) { return a * b; });
+        for (auto answer : selection)
+            return answer;
     }
-
-    return 0;
 }
 
-auto part2(const std::vector<int>& input, int target) noexcept {
+auto part2(const std::vector<int> &input, int target) noexcept {
     size_t input_size = input.size();
 
-    for(int i = 0; i < input_size; i++) {
-        for(int j = i; j < input_size; j++) {
-            for(int k = j; k < input_size; k++) {
-                if(input[i] + input[j] + input[k] == target) {
+    for (int i = 0; i < input_size; i++) {
+        for (int j = i; j < input_size; j++) {
+            for (int k = j; k < input_size; k++) {
+                if (input[i] + input[j] + input[k] == target) {
                     return input[i] * input[j] * input[k];
                 }
             }
         }
-    } 
+    }
 
     return 0;
 }
 
-
 const int TARGET = 2020;
 
 int main() {
-    std::ifstream file {"../../Inputs/day01.txt"};
+    std::ifstream file{"../../Inputs/day01.txt"};
     std::vector<int> v;
     int input;
-    while(file >> input) {
+    while (file >> input) {
         v.push_back(input);
     }
 
-    std::cout << part1(v, TARGET) << std::endl; 
-    std::cout << part2(v, TARGET) << std::endl; 
+    std::cout << part1(v, TARGET) << std::endl;
+    std::cout << part2(v, TARGET) << std::endl;
 }
