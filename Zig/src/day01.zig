4103a56c7ce757e130ea121a654b8e17da2d239f
Dodo <kasper199914@gmail.com>
2021-09-18 18:23:40 +0200

    update to better algorithm

--- Zig/src/day01.zig
+++ Zig/src/day01.zig
@@ -3,43 +3,55 @@ const input = @embedFile("../../Inputs/day01.txt");
 const std = @import("std");
 const mem = std.mem;
 const fmt = std.fmt;
-const ArrayList = std.ArrayList;
+const HashMap = std.AutoHashMap;
 
 const TARGET = @as(usize, 2020);
 
-pub fn parseInput(slice: []const u8, allocator: *mem.Allocator) !ArrayList(usize) {
-    var nums = ArrayList(usize).init(allocator);
-    errdefer nums.deinit();
+const ParseIterator = struct {
+    const Self = @This();
 
-    var split = mem.split(slice, "\r\n");
-    while (split.next()) |line| {
-        const num = try fmt.parseInt(usize, line, 10);
-        try nums.append(num);
+    split: mem.SplitIterator,
+
+    pub fn init(string: []const u8) Self {
+        return Self{ .split = mem.split(string, "\r\n") };
     }
 
-    return nums;
-}
+    pub fn next(self: *Self) ?anyerror!usize {
+        return if (self.split.next()) |line| fmt.parseInt(usize, line, 10) else null;
+    }
+};
 
-pub fn part1(slice: []const usize, target: usize) !usize {
-    for (slice) |x, idx| {
-        for (slice[idx..]) |y| {
-            if (x + y == target) {
-                return x * y;
+const Answer = struct { p1: usize, p2: usize };
+
+fn solve(iter: ParseIterator, target: usize, allocator: *mem.Allocator) !Answer {
+    var p1: ?usize = null;
+    var p2: ?usize = null;
+
+    var set = HashMap(usize, void).init(allocator);
+    defer set.deinit();
+
+    var iterator = iter;
+    while (iterator.next()) |item| {
+        const x = try item;
+
+        const search_value_p1 = target -% x;
+
+        if (set.get(search_value_p1)) |_| {
+            if (p1 == null) p1 = x * search_value_p1;
+        }
+
+        var keyiter = set.keyIterator();
+        while (keyiter.next()) |y| {
+            const search_value_p2 = target -% x -% y.*;
+            if (set.get(search_value_p2)) |_| {
+                if (p2 == null) p2 = x * y.* * search_value_p2;
             }
         }
-    } else return error.SolutionNotFound;
-}
 
-pub fn part2(slice: []const usize, target: usize) !usize {
-    for (slice) |x, idx| {
-        for (slice[idx..]) |y, idx2| {
-            for (slice[idx2..]) |z| {
-                if (x + y + z == target) {
-                    return x * y * z;
-                }
-            }
-        }
-    } else return error.SolutionNotFound;
+        try set.put(x, {});
+    }
+
+    return Answer{ .p1 = p1.?, .p2 = p2.? };
 }
 
 pub fn main() anyerror!void {
@@ -51,13 +63,8 @@ pub fn main() anyerror!void {
         std.log.info("\n\t[*] Leaked: {}\n\t[*] Bytes leaked: {}", .{ info, bytesUsed });
     }
 
-    const numbers = try parseInput(input, allocator);
-    defer numbers.deinit();
+    const parseiter = ParseIterator.init(input);
+    const answer = try solve(parseiter, TARGET, allocator);
 
-    const slice = numbers.items;
-
-    const p1 = try part1(slice, TARGET);
-    const p2 = try part2(slice, TARGET);
-
-    std.debug.print("Part 1: {}\nPart 2: {}\n", .{ p1, p2 });
+    std.debug.print("Part 1: {}\nPart2: {}\n", .{ answer.p1, answer.p2 });
 }
